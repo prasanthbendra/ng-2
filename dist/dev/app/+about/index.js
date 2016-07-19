8f21440a9fed5650aa3a17622a097fc64498fa0e
Prasanth B <prasanthb@quinnox.com>
2016-07-19 16:57:52 +0530

    Removed dist folder being tracked and added it in gitignore

--- dist/dev/app/+about/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./about.component'));
-__export(require('./about.routes'));
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC8rYWJvdXQvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7OztBQUdBLGlCQUFjLG1CQUFtQixDQUFDLEVBQUE7QUFDbEMsaUJBQWMsZ0JBQWdCLENBQUMsRUFBQSIsImZpbGUiOiJhcHAvK2Fib3V0L2luZGV4LmpzIiwic291cmNlc0NvbnRlbnQiOlsiLyoqXHJcbiAqIFRoaXMgYmFycmVsIGZpbGUgcHJvdmlkZXMgdGhlIGV4cG9ydCBmb3IgdGhlIGxhenkgbG9hZGVkIEFib3V0Q29tcG9uZW50LlxyXG4gKi9cclxuZXhwb3J0ICogZnJvbSAnLi9hYm91dC5jb21wb25lbnQnO1xyXG5leHBvcnQgKiBmcm9tICcuL2Fib3V0LnJvdXRlcyc7XHJcblxyXG4iXX0=
