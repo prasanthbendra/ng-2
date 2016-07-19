166504aa569b7a59a7824310c5f58e556620c59c
Prasanth B <prasanthb@quinnox.com>
2016-07-19 17:42:11 +0530

    Created right page component and updated links in router page

---
+++ src/client/app/+rightpage/rightpage.routes.ts
@@ -0,0 +1,10 @@
+import { RouterConfig } from '@angular/router';
+
+import { RightpageComponent } from './index';
+
+export const RightpageRoutes: RouterConfig = [
+  {
+    path: 'side-right',
+    component: RightpageComponent
+  }
+];
--- src/client/app/app.routes.ts
+++ src/client/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { provideRouter, RouterConfig } from '@angular/router';
 
+import { RightpageRoutes } from './+rightpage/index';
+import { LeftpageRoutes } from './+leftpage/index';
+import { ContactRoutes } from './+contact/index';
 import { AboutRoutes } from './+about/index';
 import { HomeRoutes } from './+home/index';
 
 const routes: RouterConfig = [
   ...HomeRoutes,
-  ...AboutRoutes
+  ...AboutRoutes,
+  ...ContactRoutes,
+  ...LeftpageRoutes,
+  ...RightpageRoutes
 ];
 
 export const APP_ROUTER_PROVIDERS = [
