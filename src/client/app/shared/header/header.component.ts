c6c4eea503b821b13bfcfda6e399360cb555af4c
Prasanth B <prasanthb@quinnox.com>
2016-07-20 19:20:36 +0530

    Header animation - Included it in header component

--- src/client/app/shared/header/header.component.ts
+++ src/client/app/shared/header/header.component.ts
@@ -5,10 +5,23 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
  * This class represents the navigation bar component.
  */
 @Component({
-  moduleId: module.id,
-  selector: 'header',
-  templateUrl: 'header.component.html',
-  styleUrls: ['header.component.css'],
-  directives: [ROUTER_DIRECTIVES]
+    moduleId: module.id,
+    selector: 'header',
+    templateUrl: 'header.component.html',
+    styleUrls: ['header.component.css'],
+    directives: [ROUTER_DIRECTIVES]
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+
+    ngOnInit() {
+        $(".headroom").headroom({
+            "tolerance": 20,
+            "offset": 50,
+            "classes": {
+                "initial": "animated",
+                "pinned": "slideDown",
+                "unpinned": "slideUp"
+            }
+        });
+    }
+}
